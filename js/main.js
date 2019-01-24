90115e6c5c233a8e6dbf9ae28fd16d1f4758c97a
Kaur <p_kaur92032@tss-hr114-dm2.fc.ca>
2019-01-24 18:27:45 -0500

    adding intial files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(() => {
+
+	var pencil = document.querySelector('#pencil');
+
+	function logMyId() {
+		console.log(#pencil);
+		// body...
+	}
+	pencil.addEventListener("click", logMyId);
+
+
+})();
